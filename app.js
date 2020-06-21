fffe2ac33ef5af18c86a3c6470a0b57355fada71
MuhammadShahbaz47 <azizterai32@gmail.com>
2020-06-21 23:56:33 +0500

    Second commit JavaScript task

---
+++ app.js
@@ -0,0 +1,62 @@
+// CHAP 21 TO 25
+
+//TASK 1
+
+// var firstname=prompt("Enter first name")
+// var lastname=prompt("Enter Last name")
+// var fullName=firstame+lastname;
+// alert("Greetings "+fullName)
+
+// TASK 2
+
+// var mob=prompt("Enter you favourite mobile company name and model")
+// var mobind=mob.length;
+// document.write("My favourite phone is: "+mob+"<br>")
+// document.write("Length of string: "+mobind)
+
+//TASK 3
+
+// var p="Pakistani"
+// var pi=p.indexOf("n")
+// document.write("String : "+p+"<br>")
+// document.write("Index of 'n' : "+pi)
+
+//TASK 4
+// var p="Hello World"
+// var pi=p.lastIndexOf("l")
+// document.write("String : "+p+"<br>")
+// document.write("Index of 'l' : "+pi)
+
+//TASK 5
+
+// var p="Pakistani"
+// var pi=p.charAt("3")
+// document.write("String : "+p+"<br>")
+// document.write("Char at index 3 : "+pi)
+
+//TASK 6
+
+// var firstname=prompt("Enter first name")
+// var lastname=prompt("Enter Last name")
+// var children = firstname.concat(" ",lastname);
+// document.write(children)
+
+//TASK 7
+
+// var city="Hyderabad";
+// var cityr=city.replace("Hyder","Islam")
+// document.write("City : "+ city +"<br>")
+// document.write("After replacement : "+cityr)
+
+//TASK 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var messageR=message.replace(/and/g,"&")
+// document.write(messageR)
+
+//TASK 9
+
+// var str="472";
+// document.write("Value : "+str+"<br>Type : String<br>")
+// Number(str)
+// document.write("Value : "+str+"<br>Type : Number")
